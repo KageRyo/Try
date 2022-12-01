2ed187e9c17566501b3ad63ed2bfe0ef4c0dc83d
KageRyo <k6.developer@gmail.com>
2022-12-01 19:26:05 +0800

    以gin實作簡易登入系統練習

---
+++ Golang/Account/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.LoadHTMLGlob("template/html/*")
+	//設定靜態資源的讀取
+	server.Static("/assets", "./template/assets")
+	server.GET("/login", LoginPage)
+	server.Run(":8888")
+}
+
+func LoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
